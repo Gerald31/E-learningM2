7a91f217cb0950680b1e32f5f200fb004c8f0bfd
devcloudarmel <devcloudarmel@gmail.com>
2024-10-28 19:35:00 +0300

    Add .

--- frontend-vuejs/src/configs.js
+++ frontend-vuejs/src/configs.js
@@ -1,5 +1,5 @@
 export const configs = {
-    BASE_URL: "http://127.0.0.1:8000/",
+    BASE_URL: "http://af631f5784eb64e3dbe44047104889b2-1549378145.af-south-1.elb.amazonaws.com:8000/",
     PREFIX: "api/",
     RELEASE: "local",
 };
