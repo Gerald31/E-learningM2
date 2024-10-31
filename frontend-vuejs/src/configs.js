ed5edc9ab68f7ce4a7fa48f247784f3472bba6a6
devcloudarmel <devcloudarmel@gmail.com>
2024-10-31 21:49:14 +0300

    E-LEARNING-APP

--- frontend-vuejs/src/configs.js
+++ frontend-vuejs/src/configs.js
@@ -1,5 +1,5 @@
 export const configs = {
-    BASE_URL: "http://ec2-13-244-159-92.af-south-1.compute.amazonaws.com/",
+    BASE_URL: "http://ec2-13-247-195-204.af-south-1.compute.amazonaws.com:8000",
     PREFIX: "api/",
     RELEASE: "local",
 };
